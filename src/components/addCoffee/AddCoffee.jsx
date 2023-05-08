1653c9ca77e1fa711bd4f4d81482cc564c57c58b
ptan01 <ptanjim01@gmail.com>
2023-05-08 13:23:04 +0600

    coffee send to backend and get response complite

--- src/components/addCoffee/AddCoffee.jsx
+++ src/components/addCoffee/AddCoffee.jsx
@@ -15,6 +15,15 @@ const AddCoffee = () => {
         
         const coffee = { name, chef, supplier, category, photo, details, test } ;
         console.log(coffee)
+        fetch('http://localhost:5000/coffee' ,{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(coffee)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
     }
 
     return (
