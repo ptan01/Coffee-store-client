59d02a9d33cddd10ecb2e4aafad928421057b096
ptan01 <ptanjim01@gmail.com>
2023-05-08 12:54:15 +0600

    coffee add form added

--- src/components/addCoffee/AddCoffee.jsx
+++ src/components/addCoffee/AddCoffee.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
 
 const AddCoffee = () => {
+
+    const handleCoffee = (e)=>{
+        e.preventDefault()
+        const form = e.target ;
+        const name = form.name.value ;
+        const chef = form.chef.value ;
+        const supplier = form.supplier.value ;
+        const category = form.category.value ;
+        const test = form.test.value ;
+        const details = form.details.value ;
+        const photo = form.photo.value ;
+        
+        const coffee = { name, chef, supplier, category, photo, details, test } ;
+        console.log(coffee)
+    }
+
     return (
         <div>
-            <h1>this is add coffee page</h1>
+            <h1 className='text-center text-4xl'>Add new coffee</h1>
+            <p className='text-center'>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal <br /> distribution of letters, as opposed to using Content here.</p>
+            <form onSubmit={handleCoffee} className='mx-auto p-3 w-full lg:w-[1320px] rounded-xl bg-stone-100'>
+                <div className='lg:flex justify-center gap-3'>
+                    <div className="form-control w-full max-w-xl">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <input type="text" placeholder="Coffee Name" name='name' className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full max-w-xl">
+                        <label className="label">
+                            <span className="label-text">Chef</span>
+                        </label>
+                        <input type="text" placeholder="Chef" name='chef' className="input input-bordered w-full " />
+                    </div>
+                </div>
+                <div className='lg:flex justify-center gap-3'>
+                    <div className="form-control w-full max-w-xl">
+                        <label className="label">
+                            <span className="label-text">Supplier</span>
+                        </label>
+                        <input type="text" placeholder="Supplier" name='supplier' className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full max-w-xl">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <input type="text" placeholder="Category" name='category' className="input input-bordered w-full " />
+                    </div>
+                </div>
+                <div className='lg:flex justify-center gap-3'>
+                    <div className="form-control w-full max-w-xl">
+                        <label className="label">
+                            <span className="label-text">Test</span>
+                        </label>
+                        <input type="text" placeholder="Test" name='test' className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full max-w-xl">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <input type="text" placeholder="Details" name='details' className="input input-bordered w-full " />
+                    </div>
+                </div>
+                <div className="form-control w-full max-w-xl mx-auto">
+                    <label className="label">
+                        <span className="label-text">Photo URL</span>
+                    </label>
+                    <input type="text" placeholder="URL" name='photo' className="input input-bordered w-full max-w-xl lg:mx-auto" />
+                </div>
+                <div className='text-center my-2'>
+                <input className='btn btn-wide' type="submit" value="Add Coffee" />
+                </div>
+            </form>
         </div>
     );
 };
